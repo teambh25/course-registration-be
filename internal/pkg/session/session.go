1ea3a8337d7aaa21850776366c065ab8cd687447
paststar <ladycar96@gmail.com>
2026-01-20 21:04:54 +0900

    fix: add cookie option

--- internal/pkg/session/session.go
+++ internal/pkg/session/session.go
@@ -3,6 +3,7 @@ package session
 import (
 	"course-reg/internal/pkg/constant"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -27,7 +28,14 @@ func SetSession(c *gin.Context, role constant.UserRole, userID uint) error {
 	session := sessions.Default(c)
 	session.Set("role", int(role))
 	session.Set("userID", userID)
-	session.Options(sessions.Options{MaxAge: 60 * 60}) // 1 hour
+	session.Options(sessions.Options{
+		MaxAge:   60 * 60,
+		Path:     "/",
+		Domain:   "",
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode, // cf. 백엔드 서버와 프론트 서버의 도메인이 다르면 Domain과 SameStie 설정 필요
+		// Secure:   isProd, // 운영 환경(HTTPS)일 때만 true
+	})
 	err := session.Save()
 	return err
 }
