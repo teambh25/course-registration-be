472eb0dbb287b65637ab0fa50f539275e18d6edf
paststar <ladycar96@gmail.com>
2026-02-06 18:01:26 +0900

    chore: remove prepare stmt for experiment

--- internal/pkg/database/database.go
+++ internal/pkg/database/database.go
@@ -17,8 +17,7 @@ import (
 
 func Setup() (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(setting.DatabaseSetting.URL), &gorm.Config{
-		PrepareStmt: true,
-		Logger:      logger.Default.LogMode(logger.Silent),
+		Logger: logger.Default.LogMode(logger.Silent),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect database: %w", err)
